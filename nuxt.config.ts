b45f6f84983630cee055498878ab3dad9b0c1fcd
星月なるこ <34113239+naruko-hstk@users.noreply.github.com>
2024-11-04 22:41:11 +0000

    feat: setup deployer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,8 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/test-utils", "@nuxt/eslint"],
   compatibilityDate: "2024-11-04",
+  app:{
+    baseURL: process.env.NODE_ENV === 'production' ? '/official-site/' : '/',
+    buildAssetsDir: '/static/'
+  }
 })
