ae8333940d3815fe5bb93478567f0619287ef8a9
星月なるこ <34113239+naruko-hstk@users.noreply.github.com>
2024-11-06 04:29:13 +0000

    feat: disable some rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,4 +5,11 @@ import prettier from "eslint-plugin-prettier/recommended"
 export default withNuxt(
   // Your custom configs here
   prettier,
+  [
+    {
+      rules: {
+        "vue/require-default-prop": "off",
+      },
+    },
+  ],
 )
