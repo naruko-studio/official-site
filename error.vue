1ccba12e81ac9981946992363ac69a473cd309f9
星月なるこ <34113239+naruko-hstk@users.noreply.github.com>
2024-11-06 04:29:00 +0000

    feat: add error page

---
+++ error.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="app-layout">
+    <AppHeader />
+    <div
+      style="
+        background-image: linear-gradient(135deg, #eab308 35%, #f59e0b 100%);
+      "
+      class="flex min-h-[90vh] items-center text-white"
+    >
+      <div class="error-container">
+        <div class="error-status-container">
+          <div class="error-status">{{ error.statusCode }}</div>
+        </div>
+        <div class="error-message-container">
+          <div class="error-message-title">
+            <p>Oh...</p>
+            <p>Something isn't right...</p>
+            <p>:(</p>
+          </div>
+          <div class="error-message-description">{{ error.message }}</div>
+          <button type="button" class="error-fix-button" @click="handleError">
+            Go back home
+          </button>
+        </div>
+      </div>
+    </div>
+    <AppFooter />
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  error: Object,
+})
+
+const handleError = () => clearError({ redirect: "/" })
+// const errorType =
+//   error.statusCode === 404
+//     ? "background-image: linear-gradient(135deg, #eab308 35%, #f59e0b 100%);"
+//     : "background-image: linear-gradient(135deg, #eab308 35%, #f59e0b 100%);"
+</script>
