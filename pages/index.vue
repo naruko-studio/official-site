f3f658cd235df33a86d6a0949390160cdaf17a83
星月なるこ <34113239+naruko-hstk@users.noreply.github.com>
2024-11-06 04:59:49 +0000

    feat: update home

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
-<script setup lang="ts"></script>
-
-<template></template>
+<template>
+  <div>
+    <p>We are なるこStudio</p>
+    <p>We make site</p>
+    <p>We make discord bot</p>
+    <p>We make twitch bot</p>
+    <p>We need more companions</p>
+  </div>
+</template>
