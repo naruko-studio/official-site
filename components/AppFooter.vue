a072337320b24c10aea8d97577d9c40b5b21a052
星月なるこ <34113239+naruko-hstk@users.noreply.github.com>
2024-11-09 06:39:01 +0800

    feat: 更新footer年份

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="app-footer">
     <UDivider>
-      Copyright &copy; 2000-2024&nbsp;
+      Copyright &copy; 2023-2024&nbsp;
       <NuxtLink to="https://naruko.studio/" target="_blank">
         なるこStudio
       </NuxtLink>
